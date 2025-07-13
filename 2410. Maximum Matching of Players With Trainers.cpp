d995430791eb256c9e6273d3c72b066814801dbe
Chirag Dhongadi <dhongadichirag@gmail.com>
2025-07-13 16:45:12 +0530

    Add POTD

---
+++ 2410. Maximum Matching of Players With Trainers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(begin(players),end(players));
+        sort(begin(trainers),end(trainers));
+        int m = players.size();
+        int n = trainers.size();
+
+        int count = 0;
+        int i = 0;
+        int j = 0;
+
+        while(i < m && j < n) {
+            if(players[i] > trainers[j]) {
+                j++;
+            }
+            else if(players[i] <= trainers[j]) {
+                count++;
+                i++;
+                j++;
+            }
+        }
+
+        return count;
+    }
+};
