59e2b28dc7c8e52c77344d1ea73e763279d5eb23
Chirag Dhongadi <dhongadichirag@gmail.com>
2025-07-28 14:24:37 +0530

    Add POTD

---
+++ 2044. Count Number of Maximum Bitwise-OR Subsets.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int countSubsets(int idx, int currOr, vector<int> &nums, int maxOr, vector<vector<int>> &t) {
+        if(idx == nums.size()) {
+            if(currOr == maxOr) {
+                return 1;
+            }
+            return 0;
+        }
+        if(t[idx][currOr] != -1) {
+            return t[idx][currOr];
+        }
+
+        //take nums[idx]
+        int takeCount = countSubsets(idx+1,currOr|nums[idx],nums,maxOr,t);
+
+        //Not take nums[idx]
+        int notTakeCount = countSubsets(idx+1,currOr,nums,maxOr,t);
+
+        return t[idx][currOr] = takeCount + notTakeCount;
+    }
+
+    int countMaxOrSubsets(vector<int>& nums) {
+        int maxOr = 0;
+        for(int &num : nums) {
+            maxOr |= num;
+        }
+
+        int n = nums.size();
+        vector<vector<int>> t(n+1, vector<int>(maxOr+1,-1));
+        int currOr = 0;
+        return countSubsets(0,currOr,nums,maxOr,t);
+    }
+};
