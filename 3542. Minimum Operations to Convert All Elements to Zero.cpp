d2d11bc158f3214f858cfd5bc52f39f60c87e72c
Chirag Dhongadi <dhongadichirag@gmail.com>
2025-11-10 15:43:48 +0530

    add POTD

---
+++ 3542. Minimum Operations to Convert All Elements to Zero.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        stack<int> st;
+
+        int n = nums.size();
+        int ops = 0;
+
+        for(int i=0; i<n; i++) {
+            while(!st.empty() && st.top() > nums[i]) {
+                st.pop();
+            }
+            if(nums[i] == 0) continue;
+
+            if(st.empty() || st.top() < nums[i]) {
+                ops++;
+                st.push(nums[i]);
+            }
+        }
+
+        return ops;
+    }
+};
