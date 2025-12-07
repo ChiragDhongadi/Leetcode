ce7124aa28e8a0288dc5fb60b149e51418f27dd1
Chirag Dhongadi <dhongadichirag@gmail.com>
2025-12-07 15:06:26 +0530

    add POTD

---
+++ 1523. Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,21 @@
+// Approach 1 :
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if(low%2 == 0) {
+            low++;
+        }
+        if(high%2 == 0) {
+            high--;
+        }
+        return (high - low)/2 + 1;
+    }
+};
+
+// Approach 2 ;
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        return (high+1)/2  - (low)/2;
+    }
+};
